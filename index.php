02de8ebae119b923edd0e2c75178cc2e9447512f
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-12-11 11:28:01 +0200

    3d comit

--- index.php
+++ index.php
@@ -1,32 +1,18 @@
 <?php
 echo "HELLO SIGMA !!!";
-
+require "Database.php";
 require "function.php";
-//1. ielagonas
-//publicet-savienat ar data base
+
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts");
 
 
-
-
-//data spurce name
-$dsn="mysql:host=localhost;port=3306;user=root;password=;dbname=blog_ipb23;charset=utf8mb4";
-
-//PFO- PHP data object
-$pdo= new PDO($dsn);
-
-//1. sagatavot vaicajumu (statement)
-
-$statement = $pdo->prepare("SELECT * FROM posts");
-
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 //dd($posts[0]["content"]);
 echo"<ul>";
-foreach($posts as $pos)
-{
-    echo "<li>" . $pos ["content"] . "</;i>";
-}
+foreach($posts as $pos){
+        echo "<li>" . $pos ["content"] . "</;i>";
+
+    }
 echo"</ul>";
 
